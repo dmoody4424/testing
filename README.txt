730f92aea6171091c5ae244c1e9a3766d663b107
Erin Moody <erinmoody@Erins-MacBook-Pro.local>
2017-12-10 13:25:38 -0800

    files for QA challenge

---
+++ README.txt
@@ -0,0 +1,6 @@
+This test suite verifies the components of the Jungle Socks order form, and verifies that sales tax is correctly applied for each state.  To run these tests:
+1. Make sure Ruby is installed.
+2. Put accpttest1.rb and testdata.config in your test directory.
+3. gem install watir-webdriver
+4. ruby accpttest1.rb
+
---
+++ accpttest1.rb
@@ -0,0 +1,42 @@
+require 'watir-webdriver'
+require 'testdata.config'
+
+browser = Watir::Browser.new :chrome
+
+$b.goto 'https://jungle-socks,herokuapp.com'
+#checks each component of page
+assert($b.table(:class, "line_item zebra").exists?), "Zebra Socks not found"
+assert($b.table(:class, "line_item lion").exists?),  "Lion Socks not found"
+assert($b.table(:class, "line_item elephant").exists?), "Elephant Socks not found"
+assert($b.table(:class, "line_item giraffe").exists?), "Giraffe Socks not found"
+assert($b.table(:text, "Ship to State").exists?), "State Selector not found"
+assert($b.input):value, "checkout").exists?), "Checkout button not found"
+
+#verifies sales tax
+config = testdata.config
+config.each do |state|
+$b.input(:id, 'line_item_quantity_zebra').send_keys "2"
+$b.input(:id, 'line_item_quantity_lion').send_keys "3"
+$b.input(:id, 'line_item_quantity_elephant').send_keys "2"
+$b.input(:id, 'line_item_quantity.giraffe').send_keys "1"
+
+$b.find_element(:text, "Ship to State")
+find_elements(:tag_name => "option")
+if option == state
+state.click
+
+$b.find_element(:value, "checkout")
+checkout.click
+a = $b.text_field(:id, "subtotal").value
+b = $b.text_field(:id, "taxes").value
+
+if b == a * value
+puts "Sales tax for " + state " correct."
+else
+puts "Sales tax for " + state " incorrect!"
+
+end
+
+$b.quit
+
+end
